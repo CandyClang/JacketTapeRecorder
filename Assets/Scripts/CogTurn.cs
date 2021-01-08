cdcc9c4551b96570e5c7b8a870398e2888b736ec
CandyClang <33882969+CandyClang@users.noreply.github.com>
2021-01-08 14:03:12 +1100

    casette cogs turn on audio play

---
+++ Assets/Scripts/CogTurn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogTurn : MonoBehaviour
+{
+    public AudioSource audioSource1, audioSource2, audioSource3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioSource1.isPlaying || audioSource2.isPlaying || audioSource3.isPlaying)
+        {
+            transform.Rotate(new Vector3(0f, 0f, 100f) * Time.deltaTime);
+        }
+    }
+}
